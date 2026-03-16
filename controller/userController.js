0fbf405fdc02a72b6824a6419bf2e3cda51851a8
InsituPeter <insitupeter@gmail.com>
2026-03-16 16:33:14 +0100

    Implement UserController

---
+++ controller/userController.js
@@ -0,0 +1,37 @@
+class UserController {
+    constructor(userService) {
+        this.userService = userService
+        this.getUserById = this.getUserById.bind(this)
+        this.updateUser = this.updateUser.bind(this)
+        this.deleteUser = this.deleteUser.bind(this)
+    }
+
+    async getUserById(req, res, next) {
+        try {
+            const user = await this.userService.getUserById(parseInt(req.params.userId))
+            res.json(user)
+        } catch (err) {
+            next(err)
+        }
+    }
+
+    async updateUser(req, res, next) {
+        try {
+            const user = await this.userService.updateUser(parseInt(req.params.userId), req.body)
+            res.json(user)
+        } catch (err) {
+            next(err)
+        }
+    }
+
+    async deleteUser(req, res, next) {
+        try {
+            await this.userService.deleteUser(parseInt(req.params.userId))
+            res.status(204).send()
+        } catch (err) {
+            next(err)
+        }
+    }
+}
+
+module.exports = UserController
